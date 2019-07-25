ed29b1b972d26bb805a6c25a31463f7d00a88129
양기현 <rlgusdid@naver.com>
2019-07-25 18:07:42 +0900

    프로젝트 생성

---
+++ src/main/java/com/shoppingmall/SpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shoppingmall/SpringbootWebserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shoppingmall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootWebserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

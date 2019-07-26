03e9b9ee815104ac8e9960bf4588c58cae12168a
양기현 <rlgusdid@naver.com>
2019-07-26 18:22:53 +0900

    yml 파일 생성 및 property 설정

--- src/main/java/com/shoppingmall/SpringbootWebserviceApplication.java
+++ src/main/java/com/shoppingmall/SpringbootWebserviceApplication.java
@@ -2,12 +2,25 @@ package com.shoppingmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class SpringbootWebserviceApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/home/ec2-user/app/config/springboot-webservice/real-application.yml";
+            /*+ "C:\\config\\real-application.yml,"
+            + "C:\\config\\local-social.yml,"
+            + "C:\\config\\real-social-set1.yml,"
+            + "C:\\config\\real-social-set2.yml";*/
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootWebserviceApplication.class, args);
+        new SpringApplicationBuilder(SpringbootWebserviceApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }

9367ba0e665df6b0615171be66e48887b409b871
양기현 <rlgusdid@naver.com>
2019-11-27 18:56:43 +0900

    아임포트 키, 벨류 설정 변경

--- src/main/java/com/shoppingmall/service/ProductOrderService.java
+++ src/main/java/com/shoppingmall/service/ProductOrderService.java
@@ -42,7 +42,7 @@ public class ProductOrderService {
     private final ProductOrderRepository productOrderRepository;
     private final ProductRepository productRepository;
     private final RestTemplate restTemplate;
-    // private final ImpProperties impProperties;
+    private final ImpProperties impProperties;
 
     /*@Value("${custom.imp.key}")
     private String imp_key;
@@ -54,8 +54,8 @@ public class ProductOrderService {
         String getTokenUrl = "https://api.iamport.kr/users/getToken";
 
         Map<String, Object> requestMap = new HashMap<>();
-        requestMap.put("imp_key", "1649704139434851");
-        requestMap.put("imp_secret", "VVn6B6yIM3Ev83Gs3bNw5nGHiLRCp8Tl4gsGm0o942Ec6lFawtHBQnj99aLzjEVnVn3inssrv6ECskNZ ");
+        requestMap.put("imp_key", impProperties.getKey());
+        requestMap.put("imp_secret", impProperties.getSecret());
 
         ResponseEntity<String> responseAccessToken = restTemplate.postForEntity(getTokenUrl, requestMap, String.class);
 

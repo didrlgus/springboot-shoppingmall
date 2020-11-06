c1af5945baf147bd8d834986606c4f6e3578da20
양기현 <rlgusdid@naver.com>
2020-11-06 22:43:25 +0900

    app:order-server 모듈 새로 생성 후 결제 완료 시 수행되는 consumer들 옮기기

--- app/order-server/src/main/java/com/shoppingmall/OrderApplication.java
+++ app/order-server/src/main/java/com/shoppingmall/OrderApplication.java
@@ -2,23 +2,21 @@ package com.shoppingmall;
 
 import com.shoppingmall.publisher.MessagePublisher;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 @ComponentScan(excludeFilters  = {@ComponentScan.Filter(
         type = FilterType.ASSIGNABLE_TYPE, classes = {MessagePublisher.class})})
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-public class MailApplication {
-
+@SpringBootApplication
+public class OrderApplication {
     private static final String APPLICATION_LOCATIONS = "spring.config.location="
             + "classpath:application.yml,"
-            + "/home/ec2-user/app/config/springboot-webservice/real-mail-application.yml";
-            //+ "C:\\config\\real-mail-application.yml";
+            + "/home/ec2-user/app/config/springboot-webservice/real-order-application.yml";
+            //+ "C:\\config\\real-order-application.yml";
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(MailApplication.class)
+        new SpringApplicationBuilder(OrderApplication.class)
                 .properties(APPLICATION_LOCATIONS)
                 .run(args);
     }

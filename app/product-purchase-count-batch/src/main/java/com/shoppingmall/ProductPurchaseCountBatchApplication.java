19cf442f8a20af99f12e519ed9d494faa4046fe8
양기현 <rlgusdid@naver.com>
2020-11-20 21:16:37 +0900

    app:product-purchase-count-batch 모듈에 main 메서드 추가

---
+++ app/product-purchase-count-batch/src/main/java/com/shoppingmall/ProductPurchaseCountBatchApplication.java
@@ -0,0 +1,23 @@
+package com.shoppingmall;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@RequiredArgsConstructor
+@EnableBatchProcessing
+@SpringBootApplication
+public class ProductPurchaseCountBatchApplication {
+
+    private static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/home/ec2-user/app/config/springboot-webservice/real-product-count-batch-application.yml";
+            //+ "C:\\config\\real-product-count-batch-application.yml";
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ProductPurchaseCountBatchApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
+    }
+}
